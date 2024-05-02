f78c1f7e14ff0bcb82a52242c6eac346c75b22e6
0xJoeMama <0xjoemama@gmail.com>
2024-05-02 13:37:03 +0300

    impr: got ready for release

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,21 @@
 plugins {
-    id("felis-dam") version "1.8.0-alpha"
+    id("felis-dam") version "1.8.1-alpha"
+    `maven-publish`
 }
 
 group = "felis"
 version = "1.0.0-alpha"
 
+loaderMake {
+    version = "1.20.6"
+}
+
+repositories {
+    mavenLocal()
+}
+
 dependencies {
-    implementation("felis:felis:1.3.0-alpha")
+    implementation("felis:felis:1.3.1-alpha")
     implementation("net.fabricmc:access-widener:2.1.0")
 }
 
@@ -17,5 +26,25 @@ kotlin {
 tasks.processResources {
     filesMatching("mods.toml") {
         expand("version" to version)
+    }
+}
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            pom {
+                artifactId = "access-widener"
+            }
+            from(components["java"])
+        }
+    }
+    repositories {
+        maven {
+            name = "Repsy"
+            url = uri("https://repo.repsy.io/mvn/0xjoemama/public")
+            credentials {
+                username = System.getenv("REPSY_USERNAME")
+                password = System.getenv("REPSY_PASSWORD")
+            }
+        }
     }
 }
--- src/main/kotlin/AccessWidenerLoaderPlugin.kt
+++ src/main/kotlin/AccessWidenerLoaderPlugin.kt
@@ -3,8 +3,6 @@ package felis.aw
 import felis.LoaderPluginEntrypoint
 import felis.ModLoader
 import felis.meta.ModMeta
-import felis.transformer.ClassContainer
-import felis.transformer.Transformation
 import net.fabricmc.accesswidener.AccessWidener
 import net.fabricmc.accesswidener.AccessWidenerClassVisitor
 import net.fabricmc.accesswidener.AccessWidenerReader
@@ -12,31 +10,39 @@ import net.peanuuutz.tomlkt.getStringOrNull
 import org.objectweb.asm.ClassReader
 import org.objectweb.asm.ClassWriter
 import org.objectweb.asm.Opcodes
+import org.slf4j.LoggerFactory
 
 object AccessWidenerLoaderPlugin : LoaderPluginEntrypoint {
+    private val logger = LoggerFactory.getLogger(AccessWidener::class.java)
     private val aw = AccessWidener()
 
     val ModMeta.accessWidener: String? get() = this.toml.getStringOrNull("access-widener")
 
     override fun onLoaderInit() {
-        val reader = AccessWidenerReader(this.aw)
-        for (aw in ModLoader.discoverer
-            .mapNotNull { it.meta.accessWidener }
-            .map { path -> ModLoader.classLoader.getResourceAsStream(path)?.use { it.readAllBytes() } }
-        ) {
-            reader.read(aw, "named")
-        }
-        ModLoader.transformer.registerTransformation(AwTransformation)
-    }
+        this.logger.info("Initializing Access Widener for 'named' namespace")
+        val awReader = AccessWidenerReader(this.aw)
 
-    object AwTransformation : Transformation {
-        override fun transform(container: ClassContainer) {
-            if (container.name in aw.targets) {
-                val reader = ClassReader(container.bytes)
+        val configCount = ModLoader.discoverer.mapNotNull { it.meta.accessWidener }.fold(0) { acc, path ->
+            val increasedAmount = ModLoader.classLoader.getResourceAsStream(path)
+                ?.use { it.readAllBytes() }
+                ?.let {
+                    awReader.read(it, "named")
+                    1
+                }
+                ?: 0
+
+            acc + increasedAmount
+        }
+
+        this.logger.info("Located $configCount access widener configuration${if (configCount == 1) "" else "s"}.")
+        ModLoader.transformer.registerTransformation {
+            if (it.name in aw.targets) {
+                logger.debug("Transforming ${it.name} with AccessWidener")
+                val reader = ClassReader(it.bytes)
                 val writer = ClassWriter(0)
                 val visitor = AccessWidenerClassVisitor.createClassVisitor(Opcodes.ASM9, writer, aw)
                 reader.accept(visitor, 0)
-                container.newBytes(writer.toByteArray())
+                it.newBytes(writer.toByteArray())
             }
         }
     }
